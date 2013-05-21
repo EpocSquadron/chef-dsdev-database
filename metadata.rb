cad9e4f923a661383f0009db906077c17ffc81f1
Daniel Poulin <dan.poulin@digitalsurgeons.com>
2013-05-21 15:45:25 -0400

    Satisfy foodcritic.

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
 name             "chef-dsdev-database"
-maintainer       "YOUR_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Daniel Poulin"
+maintainer_email "crimsonmage@gmail.com"
 license          "All rights reserved"
-description      "Installs/Configures chef-dsdev-database"
+description      "Configures various databases for DigitalSurgeons-specific recipes."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
-
+depends          "mysql"
+depends          "database"
